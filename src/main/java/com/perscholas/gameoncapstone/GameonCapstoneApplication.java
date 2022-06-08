c933e551455754f24d1e5015b338aed9b2f3747d
KillRiv <k.rivers121@gmail.com>
2022-06-08 14:28:45 -0500

    Connecting to remote repo

---
+++ src/main/java/com/perscholas/gameoncapstone/GameonCapstoneApplication.java
@@ -0,0 +1,13 @@
+package com.perscholas.gameoncapstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameonCapstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameonCapstoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/perscholas/gameoncapstone/GameonCapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perscholas.gameoncapstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameonCapstoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

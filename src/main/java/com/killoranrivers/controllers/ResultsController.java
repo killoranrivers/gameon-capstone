7c3a34de5fd6dc8c47e543841959f25bc347543c
KillRiv <k.rivers121@gmail.com>
2022-06-10 16:44:26 -0500

    Testing if commits affect page loading

--- src/main/java/com/killoranrivers/controllers/ResultsController.java
+++ src/main/java/com/killoranrivers/controllers/ResultsController.java
@@ -4,9 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class IndexController {
-    @GetMapping("/")
-    public String test() {
-        return "index";
+public class ResultsController {
+    @GetMapping("/results")
+    public String results() {
+        return "results";
     }
 }

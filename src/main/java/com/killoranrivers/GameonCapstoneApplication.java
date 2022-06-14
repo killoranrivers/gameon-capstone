582831ddcc3912c884f7ce915fa3dc9f7a9c63b9
KillRiv <k.rivers121@gmail.com>
2022-06-14 09:14:13 -0500

    Fixed Thymeleaf loading error, moved main app file into main package

--- src/main/java/com/killoranrivers/GameonCapstoneApplication.java
+++ src/main/java/com/killoranrivers/GameonCapstoneApplication.java
@@ -1,11 +1,10 @@
-package com.killoranrivers.gameoncapstone;
+package com.killoranrivers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GameonCapstoneApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(GameonCapstoneApplication.class, args);
 	}
--- src/main/java/com/killoranrivers/controllers/ResultsController.java
+++ src/main/java/com/killoranrivers/controllers/ResultsController.java
@@ -1,12 +1,13 @@
 package com.killoranrivers.controllers;
 
 import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ResultsController {
     @GetMapping("/results")
-    public String results() {
+    public String viewResults() {
         return "results";
     }
 }

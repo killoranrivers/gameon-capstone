08a3cc05fe42822fbeb5ef74cc5f9fa16d047e4c
KillRiv <k.rivers121@gmail.com>
2022-06-16 11:42:07 -0500

    Fixed Faves and Comments icon styling

--- src/main/java/com/killoranrivers/controllers/IndexController.java
+++ src/main/java/com/killoranrivers/controllers/IndexController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 public class IndexController {
     @GetMapping
-    public String test() {
+    public String getIndex() {
         return "index";
     }
 }
